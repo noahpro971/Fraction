fbc0ded71e52ce0cc844ecb7f819adac7f41b1b9
noahpro971 <62144169+noahpro971@users.noreply.github.com>
2020-04-14 17:05:27 -0400

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -3,9 +3,18 @@ public class Addition {
 
 	public Fraction Addition(Fraction Frac)
     {
+		(this.Numfrac1 + this.NumFrac2) / this.DenomFrac1 ;
+		
        
     }
     public Fraction Multiplication(Fraction Frac){
-       
-}
+	    (this.NumFrac1 * this.NumFrac2) / (this.DenomFrac1 * this.DenomFrac2);
+}      
+
+		private int NumFrac1 = 6;
+		private int DenomFrac1 =10;
+		private int NumFrac2 = 20;
+		private int DenomFrac2 =10;
+	
+	
 }
